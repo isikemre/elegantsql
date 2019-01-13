c2d4ff84681cf0d6318641213d497697dc2b4060
Emre Işık <emre@Emres-MacBook-Pro.local>
2019-01-13 03:39:01 +0300

    playing around with reflections

--- src/main/java/io/isik/elegantsql/ElegantSQL.java
+++ src/main/java/io/isik/elegantsql/ElegantSQL.java
@@ -1,6 +1,29 @@
 package io.isik.elegantsql;
 
+import io.isik.elegantsql.annotations.ElegantTable;
+import org.reflections.Reflections;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
+import static org.reflections.ReflectionUtils.*;
+
 public class ElegantSQL {
 
+    public static void main(String[] args) {
+        makeItElegant("io.isik.elegantsql");
+    }
+
+    public static void makeItElegant(String prefixPackage) {
+        Reflections reflections = new Reflections(prefixPackage);
+
+        Set<Class<?>> annotated = reflections.getTypesAnnotatedWith(ElegantTable.class);
+        annotated.forEach(elegantTableClass -> {
+            List<Field> allFields = Arrays.asList(elegantTableClass.getDeclaredFields());
+            allFields.forEach(field -> System.out.println(field.getType().getCanonicalName()));
+        });
+    }
 
 }
--- src/main/java/io/isik/elegantsql/annotations/ElegantTable.java
+++ src/main/java/io/isik/elegantsql/annotations/ElegantTable.java
@@ -1,4 +1,14 @@
 package io.isik.elegantsql.annotations;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
 public @interface ElegantTable {
+
+    String name() default "";
+
 }
